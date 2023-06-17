8834a3f9431122365f4eb4ab06328acb664a9721
TranThien <tranhuuthien98@gmail.com>
2023-06-17 11:29:10 +0700

    update

--- src/Pages/HomePage/ListMovie/CardItem.js
+++ src/Pages/HomePage/ListMovie/CardItem.js
@@ -30,30 +30,3 @@ export default function CardItem(props) {
     </div>
   );
 }
-
-{
-  /* <Card
-      hoverable
-      style={{
-        textAlign: "justify",
-      }}
-      "
-      cover={<img alt="example" className="h-60 object-cover" src={hinhAnh} />}
-    >
-      <Meta
-        title={<h2 className="text-center">{tenPhim}</h2>}
-        description={
-          <p className="h-12">
-            {moTa.length > 60 ? moTa.slice(0, 60) + "..." : moTa}
-          </p>
-        }
-      />
-      <div className="text-center mt-5">
-        <NavLink to={`/detail/${maPhim}`}>
-          <button className="bg-red-500 text-white px-3 py-2 rounded hover:text-red-500 hover:bg-white hover:border-red-600 border-solid  border">
-            Xem chi tiết
-          </button>
-        </NavLink>
-      </div>
-    </Card> */
-}
